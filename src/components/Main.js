5e6b36948e769dac76fa75dc0e334a84dfe0c5ad
Khoa Nguyen <khoatnnguyen1611@gmail.com>
2024-08-18 13:46:52 +0700

    Practice Done

--- src/components/Main.js
+++ src/components/Main.js
@@ -44,17 +44,17 @@ export default function Main() {
   }, [userGameItem]);
 
   return (
-    <div className="conainer">
+    <div className="container">
       <div className="main">
         <Result
-          user1GameItem={"Your code here"}
-          user2GameItem={"Your code here"}
-          result={"Your code here"}
+          user1GameItem={userGameItem}
+          user2GameItem={computerGameItem}
+          result={result}
         />
         <Display />
         <Choices
-          gameItems={"Your code here"}
-          handleGameItemChange={"Your code here"}
+          gameItems={gameItems}
+          handleGameItemChange={handleGameItemChange}
         />
       </div>
     </div>
